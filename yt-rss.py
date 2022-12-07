455311039d1da932a0c61d5595d605b7c3d0f1b2
Jon Heese <github@jonheese.com>
2022-12-07 13:17:19 -0500

    Refactor some stuff

--- yt-rss.py
+++ yt-rss.py
@@ -1,18 +1,40 @@
 import aniso8601
 import argparse
-import feedparser
 import httplib2
 import json
 import oauth2client
 import os
-import requests
 import smtplib
 import sys
+import time
+import traceback
+from dateutil.relativedelta import relativedelta
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime, timezone
 from googleapiclient import discovery
 from oauth2client import file, tools
+
+count = 0
+
+
+def do_list_api_call(youtube=None, endpoint_name=None, part="snippet", max_results=50, params=None,):
+    global count
+    count += 1
+    results = []
+
+    api_endpoint = getattr(youtube, endpoint_name)
+    params["part"] = part
+    params["maxResults"] = max_results
+    request = api_endpoint().list(**params)
+    while request is not None:
+        response = request.execute()
+        results.extend(response.get("items"))
+        request = api_endpoint().list_next(
+            previous_request=request,
+            previous_response=response,
+        )
+    return results
 
 
 def main(argv):
@@ -31,7 +53,6 @@ def main(argv):
     storage = file.Storage('creds.dat')
     credentials = storage.get()
 
-    yt_api_url = "https://www.googleapis.com/youtube/v3/videos?id=%s&part=contentDetails&key=%s"
     config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
         "config.json")
     with open(config_file, "r") as fp:
@@ -56,92 +77,109 @@ def main(argv):
         api_service_name, api_version, credentials=credentials
     )
 
-    items = []
-    request = youtube.subscriptions().list(
-        part="snippet",
-        maxResults=50,
-        mine=True,
+    channels = do_list_api_call(
+        youtube=youtube,
+        endpoint_name="subscriptions",
+        params={"mine": True},
     )
-    while request is not None:
-        response = request.execute()
-        items.extend(response.get("items"))
-        request = youtube.subscriptions().list_next(
-            previous_request=request,
-            previous_response=response,
-        )
 
-    #date_threshold = datetime.fromisoformat("2020-08-13T00:00:00+00:00")
-    today = datetime.today()
-    if today.month < 7:
-        date_threshold = datetime(today.year - 1, today.month + 6, today.day).replace(tzinfo=timezone.utc)
-    else:
-        date_threshold = datetime(today.year, today.month - 6, today.day).replace(tzinfo=timezone.utc)
+    date_threshold = (datetime.today() - relativedelta(months=6)).replace(tzinfo=timezone.utc)
     messages = []
     found = False
+    cancel = False
 
     # Loop through subscribed channels
-    for item in items:
-        channel_id = item.get("snippet").get("resourceId").get("channelId")
-        url = f"https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}"
-        entries = feedparser.parse(url).entries
+    for channel in channels:
+        if cancel:
+            break
+        channel_id = channel.get("snippet").get("resourceId").get("channelId")
+        uploads_playlist_id = channel_id.replace("UC", "UU", 1)
+        try:
+            videos = do_list_api_call(
+                youtube=youtube,
+                endpoint_name="playlistItems",
+                params={
+                    "playlistId": uploads_playlist_id,
+                },
+            )
+        except Exception:
+            #traceback.print_exc()
+            continue
+
         # Loop through videos in this channel's feed
-        for entry in entries:
-            livestream = int(entry.media_statistics["views"]) < 2
-            if "published" in entry.keys():
-                published_date = datetime.fromisoformat(entry.published)
-            elif "updated" in entry.keys():
-                published_date = datetime.fromisoformat(entry.updated)
-            else:
-                # Couldn't find a published/updated date, skip it and hope it's got
-                # this data next time
-                continue
+        for video in videos:
+            snippet = video.get("snippet")
+            published_date = datetime.strptime(snippet.get("publishedAt"), "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
+            video_id = snippet.get("resourceId").get("videoId")
+
+            video_url = f'https://www.youtube.com/watch?v={video_id}'
+
             # Skip videos we already know about or are older than 8/13/2020
-            if entry.link not in datastore.keys() and published_date > date_threshold:
-                try:
-                    duration_data = json.loads(requests.get(yt_api_url % (entry.yt_videoid, config["api_key"])).text)
-                    if "items" in duration_data.keys():
-                        duration = str(aniso8601.parse_duration(duration_data['items'][0]['contentDetails']['duration']))
-                    else:
-                        duration = "Unknown Duration"
-                except Exception as e:
-                    duration = "Unknown Duration"
-                if duration == "0:00:00":
-                    livestream = True
-                datastore[entry.link] = {
-                    "title": entry.title,
-                    "date": datetime.isoformat(published_date)
-                    }
-                print(f"Found new video for channel {entry.author}: {entry.title}")
-                image_html = ""
-                for thumbnail in entry.media_thumbnail:
-                    image_html += f"""<p><img src="{thumbnail['url']}"
-                    width="{thumbnail['width']} height="{thumbnail['height']}"
-                    /></p>"""
-                    if not image_html:
-                        image_html = "NO THUMBNAIL"
-                message = MIMEMultipart("alternative")
-                if livestream:
-                    message["Subject"] = f"{item['snippet']['title']} just announced a LIVE STREAM"
-                else:
-                    message["Subject"] = f"{item['snippet']['title']} just uploaded a video"
-                message["From"] = f'YouTube <{config["email"]}>'
-                message["To"] = config["email"]
-                text = f"""\
-                    {entry.title}
-                    {entry.link} ({duration})"""
-                html = f"""\
-                    <html>
-                    <body>
-                        <a href="{entry.link}">{image_html}</a>
-                        <p><a href="{entry.link}">{entry.title}</a>
-                        ({duration})</p>
-                    </body>
-                   </html>
-                   """
-                message.attach(MIMEText(text, "plain"))
-                message.attach(MIMEText(html, "html"))
-                messages.append(message)
-                found = True
+            if video_url in datastore.keys() or published_date < date_threshold:
+                continue
+
+            try:
+                streaming_details = do_list_api_call(
+                    youtube=youtube,
+                    endpoint_name="videos",
+                    part="liveStreamingDetails",
+                    params={
+                        "id": video_id
+                    },
+                )[0]
+                livestream = "liveStreamingDetails" in streaming_details and \
+                             "actualStartTime" in streaming_details["liveStreamingDetails"]
+            except Exception:
+                print(f"Unable to find streaming details for {channel['snippet']['title']}: {snippet['title']}")
+                livestream = False
+
+            try:
+                duration_data = do_list_api_call(
+                    youtube=youtube,
+                    endpoint_name="videos",
+                    part="contentDetails",
+                    params={"id": video_id},
+                )
+                duration = str(aniso8601.parse_duration(duration_data[0]['contentDetails']['duration']))
+            except Exception as e:
+                duration = "Unknown Duration"
+
+            datastore[video_url] = {
+                "title": snippet["title"],
+                "date": datetime.isoformat(published_date),
+            }
+            print(f"Found new video for channel {channel['snippet']['title']}: {snippet['title']}")
+            image_html = ""
+            thumbnail = snippet.get("thumbnails").get("high")
+            if "url" in thumbnail:
+                image_html += f"""<p><img src="{thumbnail['url']}"
+                width="{thumbnail['width']} height="{thumbnail['height']}"
+                /></p>"""
+            else:
+                image_html = "NO THUMBNAIL"
+            message = MIMEMultipart("alternative")
+            if livestream:
+                message["Subject"] = f"{channel['snippet']['title']} just announced a LIVE STREAM"
+            else:
+                message["Subject"] = f"{channel['snippet']['title']} just uploaded a video"
+            message["From"] = f'YouTube <{config["email"]}>'
+            message["To"] = config["email"]
+            text = f"""\
+                {snippet['title']}
+                {video_url} ({duration})"""
+            html = f"""\
+                <html>
+                <body>
+                    <a href="{video_url}">{image_html}</a>
+                    <p><a href="{video_url}">{snippet['title']}</a>
+                    ({duration})</p>
+                </body>
+               </html>
+               """
+            message.attach(MIMEText(text, "plain"))
+            message.attach(MIMEText(html, "html"))
+            messages.append(message)
+            found = True
 
     if found:
         with smtplib.SMTP(config["smtp_server"], config["smtp_port"]) as server:
@@ -149,8 +187,22 @@ def main(argv):
             for message in messages:
                 server.sendmail(config["email"], config["email"], message.as_string())
 
+    pruned = False
+    date_threshold = (datetime.today() - relativedelta(months=6, days=1)).replace(tzinfo=timezone.utc)
+    for key, item in datastore.copy().items():
+        timestamp = datetime.fromisoformat(item["date"]).replace(tzinfo=timezone.utc)
+        if timestamp < date_threshold:
+            del datastore[key]
+            pruned = True
+
+    if found or pruned:
+        if pruned:
+            print("Performing datastore file pruning")
         with open(config["datastore_file"], "w") as fp:
             json.dump(datastore, fp, indent=2)
+
+    global count
+    print(f"I made a total of {count} API calls")
 
 
 if __name__ == '__main__':
